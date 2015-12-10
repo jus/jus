28c07b9f8219fc40003b3af0c489a53b3fc5774b
Zeke Sikelianos <zeke@sikelianos.com>
2015-12-09 23:58:05 -0600

    refactor server and write tests for it with supertest

--- test/server.js
+++ test/server.js
@@ -1,34 +1,63 @@
 /* globals before, after, describe, it */
 
 const assert    = require('assert')
+const path      = require('path')
 const supertest = require('supertest')
 const server    = require('../lib/server')
 
 describe('server', function () {
   this.timeout(5000)
 
-  before(function(){
-    process.env.JUS_DIR = './test/fixtures'
-    console.log('I just set process.env.JUS_DIR')
-    server.jus()
+  before(function(done){
+    server.start(path.resolve(__dirname, 'fixtures'), done)
   })
 
-  // after(function(){
-  //   delete process.env.JUS_DIR
-  // })
+  describe('GET /api', function(){
+    var files
 
-  describe('GET /api/files', function(){
-    it('responds with json', function(done){
+    before(done => {
       supertest(server)
-        .get('/api/files')
-        // .set('Accept', 'application/json')
-        // .expect('Content-Type', /json/)
+        .get('/api')
+        .set('Accept', 'application/json')
         .expect(200)
-        .end(function(err, res){
-          console.log(res)
-          done()
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          files = res.body
+          files.forEach(file => files[file.href] = file)
+          return done()
         })
     })
+
+    it('responds with a JSON array of file objects', function(){
+      assert(Array.isArray(files))
+      assert(files.length)
+      assert(files['/apples'])
+      assert(files['/styles.css'])
+    })
+
+  })
+
+
+  describe('GET /api/files/apples', function(){
+    var page
+
+    before(done => {
+      supertest(server)
+        .get('/api/files/apples')
+        .set('Accept', 'application/json')
+        .expect(200)
+        .expect('Content-Type', /json/)
+        .end((err, res) => {
+          page = res.body
+          return done()
+        })
+    })
+
+    it('responds with a specific file object', function(){
+      assert.equal(page.href, '/apples')
+      assert.equal(page.title, 'Apples!')
+    })
+
   })
 
   // describe('GET /files', function(){
