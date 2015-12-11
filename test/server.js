4e47f9bfedf6c6c133804016ff402810336fbe28
Zeke Sikelianos <zeke@sikelianos.com>
2015-12-10 22:12:10 -0600

    keep log messages out of test runs

--- test/server.js
+++ test/server.js
@@ -1,9 +1,10 @@
 /* globals before, after, describe, it */
 
-const assert    = require('assert')
-const path      = require('path')
-const supertest = require('supertest')
-const server    = require('../lib/server')
+const assert      = require('assert')
+const path        = require('path')
+const supertest   = require('supertest')
+const cheerio     = require('cheerio')
+const server      = require('../lib/server')
 
 describe('server', function () {
   this.timeout(5000)
@@ -60,15 +61,25 @@ describe('server', function () {
 
   })
 
-  // describe('GET /files', function(){
-  //   it('responds with json', function(done){
-  //     supertest(server)
-  //       .get('/files')
-  //       .set('Accept', 'application/json')
-  //       .expect('Content-Type', /json/)
-  //       .expect(200, done)
-  //   })
-  // })
+  describe('GET /apples', function(){
+    var $
+
+    before(done => {
+      supertest(server)
+        .get('/apples')
+        .expect(200)
+        .end((err, res) => {
+          $ = cheerio.load(res.body)
+          return done()
+        })
+    })
+
+    it('responds with an HTML page', function(){
+      console.log($.html())
+      assert.equal($('<title>').text(), 'Apples!')
+    })
+
+  })
 
 
 })
