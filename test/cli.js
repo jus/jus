fe539fb78ab9309727dc240d328030c34699da88
Zeke Sikelianos <zeke@sikelianos.com>
2015-12-14 21:36:27 -0600

    use regular assert

--- test/cli.js
+++ test/cli.js
@@ -1,10 +1,9 @@
 /* globals describe, it */
 
-const assert = require('power-assert')
+const assert = require('assert')
 const nixt = require('nixt')
 
 describe('jus CLI', function () {
-  this.timeout(5000)
 
   it("outputs usage if run without a command", function(done) {
     nixt()
@@ -13,26 +12,25 @@ describe('jus CLI', function () {
       .end(done)
   })
 
-  describe('environment', function(){
-
-    describe('port', function() {
-      it("defaults to 3000")
-      it("can be overridden with --port")
-      it("can be overridden with -p")
-    })
-
-  })
-
-  describe('jus server', function(){
-
-    it("starts the server")//, function(done) {
-    //     nixt()
-    //       .run('./lib/cli.js serve test/fixtures')
-    //       .stdout(/Juicing/i)
-    //       .end(done)
-    // })
-
-
-  })
+  // describe('environment', function(){
+  //
+  //   describe('port', function() {
+  //     it("defaults to 3000")
+  //     it("can be overridden with --port")
+  //     it("can be overridden with -p")
+  //   })
+  //
+  // })
+  //
+  // describe('jus server', function(){
+  //
+  //   it("starts the server"), function(done) {
+  //     nixt()
+  //       .run('./lib/cli.js serve test/fixtures')
+  //       .stdout(/Juicing/i)
+  //       .end(done)
+  //   })
+  //
+  // })
 
 })
