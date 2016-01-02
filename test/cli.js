2e1cdc35a431fdbaecbb29f6c4c4cc665b294a3e
Zeke Sikelianos <zeke@sikelianos.com>
2016-01-01 22:30:56 -0600

    remove unused cli tests

--- test/cli.js
+++ test/cli.js
@@ -4,6 +4,7 @@ const assert = require('assert')
 const nixt = require('nixt')
 
 describe('jus CLI', function () {
+  this.timeout(5000)
 
   it("outputs usage if run without a command", function(done) {
     nixt()
@@ -11,26 +12,5 @@ describe('jus CLI', function () {
       .stdout(/usage/i)
       .end(done)
   })
-
-  // describe('environment', function(){
-  //
-  //   describe('port', function() {
-  //     it("defaults to 3000")
-  //     it("can be overridden with --port")
-  //     it("can be overridden with -p")
-  //   })
-  //
-  // })
-  //
-  // describe('jus server', function(){
-  //
-  //   it("starts the server"), function(done) {
-  //     nixt()
-  //       .run('./bin/cmd.js serve test/fixtures')
-  //       .stdout(/Juicing/i)
-  //       .end(done)
-  //   })
-  //
-  // })
 
 })
