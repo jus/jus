02943ecc5574e550a78e9bea8ab454819a0431f2
Zeke Sikelianos <zeke@sikelianos.com>
2016-01-10 22:00:23 -0600

    add help command and improve usage output

--- bin/cmd.js
+++ bin/cmd.js
@@ -2,6 +2,7 @@
 
 const path            = require('path')
 const tmp             = require('tmp')
+const open            = require('open')
 const server          = require('../lib/server')
 const compiler        = require('../lib/compiler')
 const args            = require('minimist')(process.argv.slice(2))
@@ -26,6 +27,10 @@ switch(command) {
   case 'squeeze':
     compiler.start(sourceDir, targetDir)
     break
+  case 'help':
+  case 'docs':
+    open('http://jus.js.org')
+    break
   default:
     console.log(`Unrecognized command: ${command}\n`)
     usage()
@@ -35,14 +40,11 @@ function usage() {
   console.log(
 `Usage:
 
-jus serve
-jus serve <sourceDir>
-jus serve <sourceDir> --port 1337
-
-jus compile <sourceDir> <targetDir>
-
-default sourceDir: .
-default port: 3000
+  jus serve                               Start server in the current directory
+  jus serve <sourceDir>                   Start server is a specific directory
+  jus serve <sourceDir> --port 1337       Start server with a custom port. Default is 3000
+  jus compile <sourceDir> <targetDir>     Compile project to static assets (HTML, JS and CSS)
+  jus help                                Open jus.js.org in your browser
 `)
   process.exit(1)
 }
