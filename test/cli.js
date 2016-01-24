b53d81369be9ca8e3a9e808a55f9e893b009da5d
Zeke Sikelianos <zeke@sikelianos.com>
2016-01-24 17:49:33 -0600

    add tests for the build command

--- test/cli.js
+++ test/cli.js
@@ -1,7 +1,10 @@
 /* globals describe, it */
 
-const nixt = require('nixt')
-const pkg = require('../package.json')
+const nixt      = require('nixt')
+const exists    = require('path-exists').sync
+const expect    = require('chai').expect
+const fse        = require('fs-extra')
+const pkg       = require('../package.json')
 
 describe('jus CLI', function () {
   this.timeout(5000)
@@ -19,5 +22,22 @@ describe('jus CLI', function () {
       .run('./cli.js -v')
       .stdout(pkg.version)
       .end(done)
+  })
+
+  it("builds", function(done) {
+    nixt()
+      .run('rm -rf test/fixtures-build')
+      .run('./cli.js build test/fixtures test/fixtures-build')
+      .stdout(pkg.version)
+      .end(function(){
+        var sourceFile = './test/fixtures/index.md'
+        expect(exists(sourceFile)).to.equal(true)
+
+        var targetFile = './test/fixtures-build/index.html'
+        expect(exists(targetFile)).to.equal(true)
+
+        fse.removeSync('./test/fixtures-build')
+        done()
+      })
   })
 })
